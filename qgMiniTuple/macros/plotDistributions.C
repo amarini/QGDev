daecf88d3833b3d4833df7ca45b7b80d76547b74
Andrea Marini <andrea.marini@cern.ch>
2016-03-11 13:34:09 +0100

    change plots to take the right input

--- qgMiniTuple/macros/plotDistributions.C
+++ qgMiniTuple/macros/plotDistributions.C
@@ -27,7 +27,8 @@ int main(int argc, char**argv){
   binClass bins = getNoBinning();
 
   // For different jet types (if _antib is added bTag is applied)
-  for(TString file : {"QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8_PU20"}){
+  //for(TString file : {"QCD_Pt-15to3000_Tune4C_Flat_13TeV_pythia8_PU20"}){
+  for(TString file : {"QCD_AllPtBins"}){
     for(TString jetType : {"AK4chs"}){
       std::cout << "Making plots for " << jetType << " in file " << file << "..." << std::endl;
       system("rm -rf plots/distributions/" + file + "/" + jetType);
@@ -38,7 +39,8 @@ int main(int argc, char**argv){
       bins.setReference("rho", &t.rho);
 
       // Init local localQGikelihoodCalculator
-      QGLikelihoodCalculator localQG("../data/pdfQG_" + jetType + "_13TeV_v1.root");
+      //QGLikelihoodCalculator localQG("../data/pdfQG_" + jetType + "_13TeV_v1.root");
+      QGLikelihoodCalculator localQG("../data/pdfQG_" + jetType + "_13TeV_76X.root");
 
       // Creation of histos
       std::map<TString, TH1D*> plots;
